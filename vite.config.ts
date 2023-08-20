b1ed146979200b71d0acede652f2fe2e70630999
KAZUAKI KADOYA <kadoya@penpale.jp>
2023-08-20 15:45:00 +0900

    Change base directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kanji/",
   plugins: [svelte()],
+  base: "/kanji_exam/",
 })
