66e6cdb4c26918479e0cc405fa9ced6183168697
KAZUAKI KADOYA <kadoya@penpale.jp>
2024-08-06 22:03:42 +0900

    ランダム選択機能追加

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
 	//fonts
 	let font = "yu-font";
-	let choosed = [];
+	let choosed:Element[] = [];
 	let showAnswer = false;
 
 	async function fetchData() {
@@ -17,26 +17,57 @@
 	}
 	fetchData();
 
-	function chooseKanji(data) {
+	function chooseKanji(data:JSON) {
 		const keys = Object.keys(data);
 		const length = keys.length;
-		const choosed = keys[Math.floor(Math.random()*length)];
+	}
+
+	function randomChoice() {
+		const numOfChoose = document.getElementById("num_of_choose") as HTMLSelectElement;
+		let num:number = 0
+		if (numOfChoose) {
+			num = Number(numOfChoose.value);
+		}
+		if (num > 0) {
+			const newChoosed:Element[] = [];
+			let indices = new Set();
+			// ランダムな20問のインデックスを選択
+			while (indices.size < num) {
+				indices.add(Math.floor(Math.random() * data.length));
+			}
+			
+			// 選ばれたインデックスに基づいて新しいchoosed配列を構築
+			indices.forEach(index => {
+				newChoosed.push(data[index]);
+			});
+
+			// Svelteに配列の変更を検出させるために、choosedを置き換える
+			choosed = newChoosed;
+		}
 	}
 </script>
 
-<main class="">
+<main class="{font}">
 	<div class="noprint">
 		<h1>漢字練習問題作成</h1>
-		<h2>フォント</h2>
+		<h2>フォント（ローカルにインストールが必要です）</h2>
 		<p class="yu-font"><label for="font-yu"><input type="radio" name="font" id="font-yu" value="yu-font" bind:group={font}>游教科書体</label></p>
 		<p class="ud-font"><label for="font-ud"><input type="radio" name="font" id="font-ud" value="ud-font" bind:group={font}>UDデジタル教科書体 ProN R</label></p>
 		
-		<h2>漢字候補（最大{max_choosed}個{#if choosed.length > 0}／{choosed.length}個 選択中{/if}）</h2>
+		<h2>漢字候補（最大{max_choosed}問{#if choosed.length > 0}／{choosed.length}問 選択中{/if}）</h2>
+		<select name="num_of_choose" id="num_of_choose">
+			<option value="">ランダム選択する数</option>
+			<option value="5">5問</option>
+			<option value="10">10問</option>
+			<option value="15">15問</option>
+			<option value="20">20問</option>
+		</select>
+		<button on:click={randomChoice}>ランダムに選ぶ</button>
 		<div class="list {font}">
 			{#if isLoading}
 				<p>Loading...</p>
 			{:else}
-			{#each data.sannen as word (word.yomi)}
+			{#each data as word (word.yomi)}
 			<p class="item">
 				<label for="{word.yomi}">
 				<input type="checkbox" name="kanji"
