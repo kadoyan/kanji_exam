ae0a983d5362e97322926a4afc307f2654ba32d2
KAZUAKI KADOYA <kadoya@penpale.jp>
2024-08-07 17:07:19 +0900

    ファイルアップロード対応

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-
 	let data = null;
 	let isLoading = true;
-	const max_choosed = 20
+	const max_choosed = 20;
 
 	//fonts
 	let font = "yu-font";
-	let choosed:Element[] = [];
+	let choosed: Element[] = [];
 	let showAnswer = false;
 
 	async function fetchData() {
@@ -17,27 +16,33 @@
 	}
 	fetchData();
 
-	function chooseKanji(data:JSON) {
+	function chooseKanji(data: JSON) {
 		const keys = Object.keys(data);
 		const length = keys.length;
 	}
 
 	function randomChoice() {
-		const numOfChoose = document.getElementById("num_of_choose") as HTMLSelectElement;
-		let num:number = 0
+		const numOfChoose = document.getElementById(
+			"num_of_choose",
+		) as HTMLSelectElement;
+		let num: number = 0;
 		if (numOfChoose) {
 			num = Number(numOfChoose.value);
 		}
 		if (num > 0) {
-			const newChoosed:Element[] = [];
+			const newChoosed: Element[] = [];
 			let indices = new Set();
-			// ランダムな20問のインデックスを選択
-			while (indices.size < num) {
+			// ランダムにインデックスを選択
+			let loop = num;
+			if (data.length < num) {
+				loop = data.length;
+			}
+			while (indices.size < loop) {
 				indices.add(Math.floor(Math.random() * data.length));
 			}
-			
+
 			// 選ばれたインデックスに基づいて新しいchoosed配列を構築
-			indices.forEach(index => {
+			indices.forEach((index) => {
 				newChoosed.push(data[index]);
 			});
 
@@ -45,45 +50,108 @@
 			choosed = newChoosed;
 		}
 	}
+
+	// カスタムJSON読み込み
+	async function uploadJson(e) {
+		const file = e.target.files[0];
+		const reader = new FileReader();
+		reader.onload = function (evt) {
+			try {
+				data = JSON.parse(evt.target.result);
+			} catch (e) {
+				return false;
+			}
+		};
+		reader.onerror = function () {
+			return false;
+		};
+		reader.readAsText(file);
+	}
 </script>
 
-<main class="{font}">
+<main class={font}>
 	<div class="noprint">
 		<h1>漢字練習問題作成</h1>
 		<h2>フォント（ローカルにインストールが必要です）</h2>
-		<p class="yu-font"><label for="font-yu"><input type="radio" name="font" id="font-yu" value="yu-font" bind:group={font}>游教科書体</label></p>
-		<p class="ud-font"><label for="font-ud"><input type="radio" name="font" id="font-ud" value="ud-font" bind:group={font}>UDデジタル教科書体 ProN R</label></p>
-		
-		<h2>漢字候補（最大{max_choosed}問{#if choosed.length > 0}／{choosed.length}問 選択中{/if}）</h2>
-		<select name="num_of_choose" id="num_of_choose">
-			<option value="">ランダム選択する数</option>
-			<option value="5">5問</option>
-			<option value="10">10問</option>
-			<option value="15">15問</option>
-			<option value="20">20問</option>
-		</select>
-		<button on:click={randomChoice}>ランダムに選ぶ</button>
+		<p class="yu-font">
+			<label for="font-yu"
+				><input
+					type="radio"
+					name="font"
+					id="font-yu"
+					value="yu-font"
+					bind:group={font}
+				/>游教科書体</label
+			>
+		</p>
+		<p class="ud-font">
+			<label for="font-ud"
+				><input
+					type="radio"
+					name="font"
+					id="font-ud"
+					value="ud-font"
+					bind:group={font}
+				/>UDデジタル教科書体 ProN R</label
+			>
+		</p>
+		<div>
+			<h2>JSONをアップロード</h2>
+			<input
+				type="file"
+				name="json"
+				id="custom_json"
+				on:change={uploadJson}
+				accept=".json"
+			/>
+		</div>
+
+		<h2>
+			漢字候補（最大{max_choosed}問{#if choosed.length > 0}／{choosed.length}問
+				選択中{/if}）
+		</h2>
+		<div>
+			<select name="num_of_choose" id="num_of_choose">
+				<option value="">ランダム選択する数</option>
+				<option value="5">5問</option>
+				<option value="10">10問</option>
+				<option value="15">15問</option>
+				<option value="20">20問</option>
+			</select>
+			<button on:click={randomChoice}>ランダムに選ぶ</button>
+		</div>
 		<div class="list {font}">
 			{#if isLoading}
 				<p>Loading...</p>
 			{:else}
-			{#each data as word (word.yomi)}
-			<p class="item">
-				<label for="{word.yomi}">
-				<input type="checkbox" name="kanji"
-					value="{word}"
-					id="{word.yomi}"
-					bind:group={choosed}
-					disabled={choosed.length === max_choosed && !choosed.includes(word)}
-				>
-				{word.kanji}<span class="kana">（{word.yomi}）</span>
-				</label>
-			</p>
-			{/each}
+				{#each data as word (word.yomi)}
+					<p class="item">
+						<label for={word.yomi}>
+							<input
+								type="checkbox"
+								name="kanji"
+								value={word}
+								id={word.yomi}
+								bind:group={choosed}
+								disabled={choosed.length === max_choosed &&
+									!choosed.includes(word)}
+							/>
+							{word.kanji}<span class="kana">（{word.yomi}）</span
+							>
+						</label>
+					</p>
+				{/each}
 			{/if}
 		</div>
 		<h2>答え</h2>
-		<label for="show_answer"><input type="checkbox" name="show_answer" id="show_answer" bind:checked={showAnswer}>答えも印刷する</label>
+		<label for="show_answer"
+			><input
+				type="checkbox"
+				name="show_answer"
+				id="show_answer"
+				bind:checked={showAnswer}
+			/>答えも印刷する</label
+		>
 	</div>
 
 	<div class="paper {font}">
@@ -94,9 +162,11 @@
 			{:else}
 				{#each choosed as item, index}
 					<div class="item">
-						<div class="kana"><span class="index">({index+1})</span>{item.yomi}</div>
+						<div class="kana">
+							<span class="index">({index + 1})</span>{item.yomi}
+						</div>
 						<div class="kanji_box">
-							{#each Array.from({length: item.kanji.length}) as _, i}
+							{#each Array.from( { length: item.kanji.length }, ) as _, i}
 								<div class="kanji"></div>
 							{/each}
 						</div>
@@ -109,7 +179,7 @@
 				<h3 class="noprint">回答</h3>
 				{#each choosed as item, index}
 					<div class="item">
-						<span class="index">({index+1})</span>{item.kanji}
+						<span class="index">({index + 1})</span>{item.kanji}
 					</div>
 				{/each}
 			</div>
