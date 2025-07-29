9c1cdd58ab49ad4507b8bc8b7f438b2fecbe36ac
KAZUAKI KADOYA <kadoya@penpale.jp>
2025-07-29 14:11:18 +0900

    Update to Svelte 5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import './app.scss'
-import App from './App.svelte'
+import "./app.scss";
+// import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+// const app = new App({
+//   target: document.getElementById('app'),
+// })
 
-export default app
+import { mount } from "svelte";
+import App from "./App.svelte";
+
+const app = mount(App, { target: document.getElementById("app") });
+
+export default app;
